bf21fb902af2b2f14db47137afd7527dec7beeff
Sergey Malinkin <sloyz@yandex.ru>
2020-09-07 13:35:53 +0300

    Вызов функции из функции

---
+++ backend-developer/Основы Python/Функции/Вызов функции из функции.py
@@ -0,0 +1,32 @@
+# Измените код Анфисы. Оставьте вывод на экран, т.е. вызовы функции print(), только в одном месте —
+# в основном теле программы. Эти вызовы уже написаны в предварительном коде задания.
+# Все прошлые выводы на экран — в функциях show_count_friends() и process_query() —
+# замените на возвращения результатов из функции оператором return.
+
+FRIENDS = ['Серёга', 'Соня', 'Дима', 'Алина', 'Егор']
+
+def show_count_friends(count_friends):
+    if count_friends == 1:
+        return 'У тебя 1 друг'
+    elif 2 <= count_friends <= 4:
+        return 'У тебя ' + str(count_friends) + ' друга'
+    elif count_friends >= 5:
+        return 'У тебя ' + str(count_friends) + ' друзей'
+    return count_friends
+
+
+def process_query(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(FRIENDS)
+        show_count_friends(count)
+    elif query == 'Кто все мои друзья?':
+        friends_string = ', '.join(FRIENDS)
+        return 'Твои друзья: ' + friends_string
+    else:
+        return '<неизвестный запрос>'
+    return show_count_friends(count)
+
+# Внимание! Это те самые вызовы, которые не надо трогать
+print(process_query('Сколько у меня друзей?'))
+print(process_query('Кто все мои друзья?'))
+print(process_query('Как меня зовут?'))
